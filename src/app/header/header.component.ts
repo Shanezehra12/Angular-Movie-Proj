be0bb07bf331961ba5ed32c152238f345a2e2bc3
Shanezehra12 <Shanelakhani52@gmail.com>
2023-10-10 20:23:17 +0500

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private router: Router, private auth: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  goToHome() {
+    this.router.navigate(['home']);
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+}
